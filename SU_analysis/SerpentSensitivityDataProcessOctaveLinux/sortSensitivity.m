a8ff0cb804590430c112154ec425012867ee0ad6
lijin <lijin.thu@outlook.com>
2017-11-20 13:39:45 +0800

    Add Octave version code for Linux

---
+++ SU_analysis/SerpentSensitivityDataProcessOctaveLinux/sortSensitivity.m
@@ -0,0 +1,20 @@
+for i=1:471
+    sens_max(i,1)=abs(sum(sens_nr(i,:))); % sort by energy integrated absolote value
+end
+% sort sensitivity data by descend order
+% sens_index is the index order, which index is the same as that of
+% commaraNuclideName and of commaraReactionName
+[sens_sort, sens_index] = sort(sens_max, 'descend');
+for j = 1:471
+    sens_mx(j, :) = sens_nr(sens_index(j, 1), :);
+    sens_nreg_sort(j, 1) = sum(sens_mx(j,:));
+    sens_m_index(j, 1) = sens_index(j, 1);
+end
+sens_m_indexi = int16(sens_m_index);
+
+recdf2 = fopen([tempdir,'/sensitivitySort.result'], 'wt');
+fprintf(recdf2,'No. Index   Parameter     Sensitivity(Sum of all energy bins)\n');
+for i = 1:471
+    fprintf(recdf2, '%-3d %-7d %-17s %-12g\n',i, sens_m_indexi(i, 1), [cell2mat(commaraNuclideName(sens_m_indexi(i, 1))),'-',cell2mat(commaraReactionName(sens_m_indexi(i, 1)))],sens_nreg_sort(i,1));
+end
+fclose(recdf2);
---
+++ SU_analysis/SerpentSensitivityDataProcessOctaveLinux/whichCommaraReaction.m
@@ -0,0 +1,29 @@
+function reaction=whichCommaraReaction(r)
+%Script file: serpentSensitivityDataProcess.m
+%
+%Purpose:
+%This script is used to judge which reaction data is pertubed.
+%
+%Author: Jin Li
+%Date: November 18, 2017
+switch r
+    case 'ela scatt xs        '
+        reaction='ELASTIC';
+    case 'inl scatt xs        '
+        reaction='INELASTIC';
+    case 'capture xs          '
+        reaction='CAPTURE';
+    case 'fission xs          '
+        reaction='FISSION';
+    case 'nubar total         '
+        reaction='NU';
+    case 'nxn xs              '
+        reaction='NxN';
+    case 'chi total           '
+        reaction='KHI';
+    case 'ela leg mom 1       '
+        reaction='P1ELAS';
+%     otherwise
+%         error('Unknow reaction type!');
+end
+            
